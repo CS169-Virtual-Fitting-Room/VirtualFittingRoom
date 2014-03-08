e68c0825cda04f74f2dda02d164aba05a5e81749
jackyhuo <jacky.huo@live.cn>
2014-03-08 14:41:06 -0800

    Initial idea, pls check correctness

--- main/models.py
+++ main/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from time import datatime
+class User (models.Model):
+	name = models.CharField(max_length = 128, primary_key = True)
+	##password = models.CharField(max_length = 128)
+	count = models.IntegerField(default = 0)
+	signUpTime = models.DataField()
+	lastLogInTime = models.DateField()
+	userImage = models.CharField(max_length = 128)
+	def __unicode__(self):
+	    return self.name
 
+class WishList(models.Model):
+	owner = models.OneToOneField(User, primary_key = True)
+
+	product = models.CharField(max_length = 128)
+	#createTime = models.DateField()
+
+
+class Product(models.Model):
+	owner = models.ForeignKey(User)  
+	category = models.ForeignKey(Category)
+	wishList = models.ForeignKey(WishList)
+
+	name = models.CharField(max_length = 128, primary_key = True)
+	brand = models.CharField(max_length = 128)
+	url = models.CharField(max_length = 128)
+	timeAdded = models.DataField()
+
+	photo = models.CharField(max_length = 128)
+	def __unicode__(self):
+	    return self.name
+	    
+class Comment(models.Model):
+	owner = models.ForeignKey(User)
+	productCommented = models.ForeignKey(Product)
+
+	content = models.CharField(max_length = 128)
+	##timeAdded = models.DataField()
+
+
+class Category(models.Model):
+	name = models.CharField(max_length = 128, primary_key = True)
+	photo = models.CharField(max_length = 128)
+
+	def __unicode__(self):
+	    return self.name
 # Create your models here.
--- main/views/commentViews.py
+++ main/views/commentViews.py
@@ -1,1 +1,25 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt 
 
+
+@csrf_exempt
+def addComment(request):   ## userID, productID, content
+	if request.method == 'POST' && request.META['CONTENT_TYPE'] != 'application/json'::
+		content = json.loads(request.body)
+		dataUser = dataBaseModel()
+		response = dataUser.addComment(content['owner'], content['productCommented'], content['content'])
+        ##if response[0] == UsersModel.SUCCESS: # success
+		outcome = json.dumps({'errCode' : response[0]})
+		return HttpResponse(outcome, content_type = 'application/json')
+
+
+def getComment(request):
+	if request.method == 'POST' && request.META['CONTENT_TYPE'] != 'application/json'::
+		content = json.loads(request.body)
+		dataUser = dataBaseModel()
+		response = dataUser.getComment(content['owner'], content['productCommented'])
+		commentList = response[0]
+		outcome = json.dumps({'commentList':commentList, 'errCode' : response[0]})
+		return HttpResponse(outcome, content_type = 'application/json')
+	
