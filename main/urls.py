c7f7b06848d88e799100e567ed0c12a9196f5b28
Samuel Razzell <samuelrazzell@comcast.net>
2014-03-12 16:01:01 -0700

    Created different pages, changed category list to glasses, headphones, and hats, and added header to all pages statically. Separated top menu from index.

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, url, include
 from views.mainViews import index, members, logout, mainpage
-from views.categoryViews import category_list, item
+from views.categoryViews import glasses, hats, headphones, item, top_menu
 
 urlpatterns = patterns('', url(r'', include('social_auth.urls')),
                        url(r'^members', members, name='members'),
                        url(r'^logout', logout, name='logout'),
                        url(r'^mainpage', mainpage, name='mainpage'),
-                       url(r'^category_list', category_list, name='category_list'),
+                       url(r'^glasses', glasses, name='glasses'),
+                       url(r'^hats', hats, name='hats'),
+                       url(r'^headphones', headphones, name='headphones'),
                        url(r'^item', item, name='item'),
+                       url(r'^top_menu', top_menu, name='top_menu'),
                        url(r'^$', index, name='index'),
 )   
