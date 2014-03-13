bf38a97bd63706e398c54caf3c9d9f47a9f058ba
jackyhuo <jacky.huo@live.cn>
2014-03-13 01:35:41 -0700

    add testGetProduct and testGetDetail

--- main/tests.py
+++ main/tests.py
@@ -24,86 +24,43 @@ class testUnit(TestCase):
     ERR_BAD_PRODUCTID = -11
     ERR_BAD_CATEGORYID = -12
 
-### test getProducts(productID) ###
+
     testProducts = ["aaaa","bbbb","cccc","dddd"]
+    testUsers = ["a", "b", "c", "d"]
+    testCategory = ["hat"]
     def setUp(self):
+        for i in range(4):
+            newOne = User(user = testUnit.testUsers[i], user_image = "null")
+            newOne.save()
+
         for i in range(4):
             newOne = Product(category = "hats", name = testUnit.testProducts[i], brand = "null", \
                              url = "null", photo = "null", price = 1.0 )
             newOne.save()
         for i in range(4):
-            newOne = Comment()
+            newOne = Comment(product = testUnit.testProducts[i], owner = testUnit.testUsers[i], content = "null")
+            newOne.save()
 
+
+### test getProducts(productID) ###
     def testGetProduct(self):
         baseModel = dataBaseModel()
         products = baseModel.getProducts("hats")
         for i in range(4):
             self.assertTrue(testUnit.testProducts[i] in products[0] and products[1] == testUnit.SUCCESS, "addProduct failed")
+        self.assertTrue("eeee" not in products[0], "addProduct failed")
+
+    def testGetDetail(self):
+        baseModel = dataBaseModel()
+        for i in range(4):
+            productDetail = baseModel.getDetail(testUnit.testProducts[i])
+            self.assertTrue(productDetail != None, "getDetail failed, can not find the product")
+            self.assertTrue(productDetail.category == "hats", "getDetail failed, wrong category")
+            self.assertTrue(productDetail.brand == "null", "getDetail failed, wrong brand")
+            self.assertTrue(productDetail.url == "null", "getDetail failed, wrong url")
+            self.assertTrue(productDetail.photo == "null", "getDetail failed, wrong photo")
+            self.assertTrue(productDetail.price == 1.0, "getDetail failed, wrong price")
+        productDetail = baseModel.getDetail("eeee")
+        self.assertTrue(productDetail == None, "getDetail failed, false product")
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def setUp(self):
-        newOne = User(user = 'abcd', password = 'abcd' )
-        newOne.save()
-
-    def testLogin(self):   ### good one
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.login('abcd', 'abcd'), (UsersModel.SUCCESS, self.Good_Num ))  ### count = 2
-
-    def testAdd(self):    ### good one
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add('123', '123'), (UsersModel.SUCCESS, self.Good_Num))
-
-    def testLoginWrongPassWord(self): #### Error
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.login('123', '456'), (UsersModel.ERR_BAD_CREDENTIALS, self.Err_Num))
-
-    def testNullNull(self): #### Error
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.login('', ''), (UsersModel.ERR_BAD_USERNAME, self.Err_Num))
-
-    def testAddNullUserName(self):  ### Error
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add('', '456'), (UsersModel.ERR_BAD_USERNAME, self.Err_Num))
-
-    def testAddNullPassWord(self):  ### good one
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add('abc', ''), (UsersModel.SUCCESS, self.Good_Num))
-
-    def testAddLongUserName(self):  ## Error
-        user = '1' * 130
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add(user, '1234'), (UsersModel.ERR_BAD_USERNAME, self.Err_Num))
-
-    def testAddDuplicateUserName(self):### Error
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add('abcd', '456'), (UsersModel.ERR_USER_EXISTS, self.Err_Num))
-
-    def testAddLongPassWord(self): ## Error
-        password = '1' * 200
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.add('kkkk', password), (UsersModel.ERR_BAD_PASSWORD, self.Err_Num))
-
-
-    def testRestartFixture(self):
-        dataUser = UsersModel()
-        self.assertEqual(dataUser.TESTAPI_resetFixture(), UsersModel.SUCCESS)
-        try:
-            getUser = User.objects.get(user = '123')
-            self.assertEqual(True, False)
-        except:
-            self.assertEqual(True, True)
