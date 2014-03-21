8cc1b317f57eab23ac9d76454a5166f3e6494fc6
eddielau <eddielau224@gmail.com>
2014-03-20 17:17:48 -0700

    Changed url patterns and tests

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, url, include
 from views.mainViews import index, members, logout, mainpage, top_menu, fittingroom,qunit
 from views.categoryViews import listProduct, category_list
 from views.productViews import detail, detailpage
+from views.commentViews import addComments, getComments
 
 urlpatterns = patterns('', url(r'', include('social_auth.urls')),
                        url(r'^members', members, name='members'),
@@ -13,6 +14,7 @@ urlpatterns = patterns('', url(r'', include('social_auth.urls')),
                        url(r'^$', index, name='index'),
                        url(r'^(?P<category>[\w ]+)/$', category_list, name='category_list'),
                        url(r'^(?P<category>[\w ]+)/list/$' , listProduct, name='listProduct' ),
-                       url(r'^(?P<category>[\w ]+)/(?P<product>[\w ]+)/$', detailpage, name='detailpage'),
-                       url(r'^(?P<category>[\w ]+)/(?P<product>[\w ]+)/info/$', detail, name='detail'),
+                       url(r'^(?P<category>[\w ]+)/(?P<product>[\w ]+)_(?P<id>[\d]+)/$', detailpage, name='detailpage'),
+                       url(r'^(?P<category>[\w ]+)/(?P<product>[\w ]+)_(?P<id>[\d]+)/info/$', detail, name='detail'),
+                       #url(r'^(?P<category>[\w ]+)/(?P<product>[\w ]+)/comments/$', getComments, name='getComments'),
 )   
