b0c37af97136376c46c6f58876f6ca926448795e
eddielau <eddielau224@gmail.com>
2014-03-28 15:13:35 -0700

    Fixed fitlistView

--- main/views/fitlistViews.py
+++ main/views/fitlistViews.py
@@ -27,7 +27,7 @@ def getFitlist(request):
         
 @csrf_exempt
 def addToFitlist(request, category, product, id):
-    if not ('application/json' in request.META['CONTENT_TYPE'] and request.user.is_authenticated()):
+    if not (request.user.is_authenticated()):
         return HttpResponse(json.dumps({'errCode' : dataBaseModel.ERR_BAD_REQUEST}), content_type='application/json')
     db = dataBaseModel()
     result = db.addToFitList(request.user.id, product, id)
@@ -35,7 +35,7 @@ def addToFitlist(request, category, product, id):
     
 @csrf_exempt
 def removeFromFitlist(request, category, product, id):
-    if not ('application/json' in request.META['CONTENT_TYPE'] and request.user.is_authenticated()):
+    if not (request.user.is_authenticated()):
         return HttpResponse(json.dumps({'errCode' : dataBaseModel.ERR_BAD_REQUEST}), content_type='application/json')
     db = dataBaseModel()
     result = db.removeFromFitList(request.user.id, product, id)
