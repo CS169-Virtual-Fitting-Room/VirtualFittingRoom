5d22aff1cbf48ba0f8ea64e2094a240d8ec2b8d8
eddielau <eddielau224@gmail.com>
2014-03-28 17:49:47 -0700

    Added overlay attribute

--- main/views/internalViews.py
+++ main/views/internalViews.py
@@ -19,11 +19,11 @@ def setUpDb(request):
     hats.save()
     headphones.save()
     
-    rayban = Product(category = glasses, name='rayban glasses', brand = 'rayban', url='www.rayban.com', photo='rayban.jpg', price = 129.9, description='stylish rayban')
+    rayban = Product(category = glasses, name='rayban glasses', brand = 'rayban', url='www.rayban.com', photo='rayban.jpg', overlay='rayban.ol.jpg', price = 129.9, description='stylish rayban')
     nike = Product(category = glasses, name='nike glasses', brand = 'nike', url='www.nike.com', photo='nike.jpg', price = 99.9, description = 'sporty nike')
-    adidas = Product(category = hats, name='adidas cap', brand = 'adidas', url='www.adidas.com', photo='addidas.jpg', price = 56.9, description ='adidas cap!')
+    adidas = Product(category = hats, name='adidas cap', brand = 'adidas', url='www.adidas.com', photo='addidas.jpg', overlay='addidasol.jpg', price = 56.9, description ='adidas cap!')
     levis = Product(category = hats, name='levis hat', brand = 'levis', url='www.levis.com', photo='levis.jpg', price = 67.9, description ='levis hat!')
-    beats = Product(category = headphones, name='beats headphones', brand = 'beats', url='www.beats.com', photo='beats.jpg', price = 256.9, description='stylish headphones!')
+    beats = Product(category = headphones, name='beats headphones', brand = 'beats', url='www.beats.com', photo='beats.jpg', overlay='beatsol.jpg', price = 256.9, description='stylish headphones!')
     sony = Product(category = headphones, name='sony headphones', brand = 'sony', url='www.sony.com', photo='sony.jpg', price = 399.9, description='high quality headphones!')
     rayban.save()
     nike.save()
@@ -38,7 +38,7 @@ def setUpDb(request):
     wish = WishList(owner=AUser.objects.get(pk=1), product=rayban)
     wish.save()
     
-    fit = FitList(owner=AUser.objects.get(pk=1), product=nike)
+    fit = FitList(owner=AUser.objects.get(pk=1), product=adidas)
     fit.save()
     
     return HttpResponse("Success!")
