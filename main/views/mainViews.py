3fe3f9b2a4033d7338180720b1c510316beddf8f
eddielau <eddielau224@gmail.com>
2014-04-23 18:59:31 -0700

    Changed login/logout so that it redirects to last revisited page

--- main/views/mainViews.py
+++ main/views/mainViews.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.contrib.auth import logout as google_logout
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
+from django.template import RequestContext
 
 def index(request):
     return render(request, 'main/index.html')
@@ -13,13 +14,13 @@ def members(request):
 
 def logout(request):
     google_logout(request)
-    return HttpResponseRedirect('/')
+    return HttpResponseRedirect(request.GET["next"])
 
 def mainpage(request):
     return render(request, 'main/mainpage.html')
 
 def top_menu(request):
-    return render(request, "main/top_menu.html")
+    return render(request, "main/top_menu.html", {'path' : request.META['HTTP_REFERER']})
 
 def qunit(request):
     return render(request, "main/qunit_test.html")
