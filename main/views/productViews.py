79722b0d7a0c98b3e94f9ed43d70e89817734f2c
eddielau <eddielau224@gmail.com>
2014-04-28 18:16:37 -0700

    Added brand in detail page json response

--- main/views/productViews.py
+++ main/views/productViews.py
@@ -18,11 +18,11 @@ def detail(request, category, product, id):
     db = dataBaseModel()
     item = db.getDetail(product.lower(), id)
     if item[1] != dataBaseModel.SUCCESS:
-        failData = {'image' : '', 'item_name': '', 'price' : -1, 'description' : ''}
+        failData = {'image' : '', 'item_name': '', 'price' : -1, 'description' : '', 'brand' : ''}
         return HttpResponse(json.dumps(failData), content_type='application/json')
     item = item[0]
     image = static("products/" + item.photo)
-    data = {'image' : image, 'item_name': item.name.title(), 'price' : item.price, 'description' : item.description}
+    data = {'image' : image, 'item_name': item.name.title(), 'price' : item.price, 'description' : item.description, 'brand' : item.brand}
     
     return HttpResponse(json.dumps(data ,encoding='latin-1'), content_type='application/json')
 
