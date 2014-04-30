2a75255da11c6ebe5ad88d41a1feb0d4cb721b11
jackyhuo <jacky.huo@live.cn>
2014-04-30 03:20:14 -0700

    update page option

--- main/views/searchProductViews.py
+++ main/views/searchProductViews.py
@@ -8,7 +8,7 @@ import json
 def search_list(request):
     value = request.GET.get("searchName")
     print value
-    return render(request, 'main/searchResult.html', {"searchName" : value})
+    return render(request, 'main/searchResult.html', {"searchName" : value} )
 
 
 # def searchProducts(request):
@@ -41,6 +41,11 @@ def search_list(request):
 
 def searchProducts(request):
     searchProduct = request.GET.get("searchName")
+    searchNum = int(request.GET.get("sth"))
+    pageNum = int(request.GET.get("page"))
+    print "searchNum is ", searchNum
+    print "searchProduct is ",searchProduct
+    print "pageNum is", pageNum
     id = []
     image = []
     item_name = []
@@ -52,12 +57,25 @@ def searchProducts(request):
         return HttpResponse(json.dumps(data ,encoding='latin-1'), content_type='application/json')
     else:
         items = query[0]
+        if searchNum != 0:
+            productNum = len(items)
+            print "productNum is ",productNum
+            endValue = pageNum * productNum / searchNum
+        i = 0
         for item in items:
-            id.append(item.pk)
-            item_name.append(item.name)
-            price.append(item.price)
-            image.append(static("products/" + item.photo))
-        data = {'id': id, 'image' : image, 'item_name' : item_name, 'price' : price}
+            if searchNum == 0 or  ( (pageNum -1)*searchNum <= i < (pageNum-1) * searchNum + searchNum ) :
+                id.append(item.pk)
+                item_name.append(item.name)
+                price.append(item.price)
+                image.append(static("products/" + item.photo))
+            i = i + 1
+        if searchNum == 0:
+            data = {'id': id, 'image' : image, 'item_name' : item_name, 'price' : price, 'page':1}
+        elif productNum % searchNum == 0:
+            data = {'id': id, 'image' : image, 'item_name' : item_name, 'price' : price, 'page': productNum / searchNum}
+        else:
+            data = {'id': id, 'image' : image, 'item_name' : item_name, 'price' : price, 'page':  productNum / searchNum + 1}
+
         return HttpResponse(json.dumps(data ,encoding='latin-1'), content_type='application/json')
 
 
