3fbe394b1b58fdec0b156a1534b7a49c2d57362b
kaleongkong <kaleongkong@yahoo.com>
2014-05-02 12:31:35 -0700

    fix comment profile pic

--- main/static/main/assets/js/detailpage.js
+++ main/static/main/assets/js/detailpage.js
@@ -13,11 +13,10 @@
 		return true;
 	}*/
 
-	function rendercomment(content, name, time){
+	function rendercomment(content, name, time, url){
 		if(jQuery.type(content)!="string" || jQuery.type(name)!="string" ||jQuery.type(time)!="string"){
 			return false;
 		}
-		var url = '/static/main/assets/img/anonymous-user.png';
 		var comment = '<li class="cmmnt"><div class="avatar"><img src='+url+' width="55" height="55" alt="'+name+'"></div><div class="cmmnt-content"><header>'+name+' - <span class="pubdate">posted '+time+'</span></header><p>'+content+'</p></div></li>';
 		$("#comment-list").append(comment);
 		return true;
